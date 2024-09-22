c402c398e72281dfa74754e32c6e54cdaae87408
Aguru Itabashi <itabashi.a@gmail.com>
2024-09-22 14:13:29 +0900

    テストを仮実装

--- rails/spec/rails_helper.rb
+++ rails/spec/rails_helper.rb
@@ -65,4 +65,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # これ設定しないとテスト通らない
+  config.before(:each) do
+    host! "localhost"
+  end
 end
